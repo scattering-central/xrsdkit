77d8500d4a3182a78015c8d79889f2efd08e9428
Lenson Pellouchoud <lensonp@gmail.com>
2017-11-01 10:11:52 -0700

    setup.py, related .gitignore updates

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='saxskit',
+    version='0.0.1',
+    url='https://github.com/scattering-central/saxskit.git',
+    description='Data-driven SAXS analysis',
+    author='SSRL',
+    author_email='paws-developers@slac.stanford.edu',
+    packages=find_packages(),
+    install_requires=[
+        'numpy','scikit-learn'
+    ],
+)
