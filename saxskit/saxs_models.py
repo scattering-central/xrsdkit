3a26b0b230347e5d5b1cc7930c1efe87bf165a23
Lenson Pellouchoud <lensonp@gmail.com>
2017-11-15 14:00:18 -0800

    added skeleton method for training regressors

--- saxskit/saxs_models.py
+++ saxskit/saxs_models.py
@@ -14,3 +14,19 @@ def train_classifiers(cit_client,dsid_list=[],yaml_filename=None):
     """
     pass
 
+
+def train_regressors(cit_client,dsid_list=[],yaml_filename=None):
+    """Train and save SAXS regression models as a YAML file.
+
+    Parameters
+    ----------
+    cit_client : citrination_client.CitrinationClient
+        A python Citrination client for fetching data
+    dsid_list : list of int
+        List of dataset id (integers) for fetching SAXS records
+    yaml_filename : str
+        File where scalers and models will be saved.
+        If None, the default file is used.
+    """
+    pass
+
