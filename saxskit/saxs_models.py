b743894a89c490652dcedf5ac6b182b167451d96
Lenson Pellouchoud <lensonp@gmail.com>
2017-11-15 13:54:28 -0800

    template method for generating classification models from a Citrination client

---
+++ saxskit/saxs_models.py
@@ -0,0 +1,16 @@
+
+def train_classifiers(cit_client,dsid_list=[],yaml_filename=None):
+    """Train and save SAXS classification models as a YAML file.
+
+    Parameters
+    ----------
+    cit_client : citrination_client.CitrinationClient
+        A python Citrination client for fetching data
+    dsid_list : list of int
+        List of dataset id (integers) for fetching SAXS records
+    yaml_filename : str
+        File where scalers and models will be saved.
+        If None, the default file is used.
+    """
+    pass
+
