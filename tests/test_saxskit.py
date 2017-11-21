289f68305ad7579968f63349554d86df1d08bafc
Lenson Pellouchoud <lensonp@gmail.com>
2017-11-21 08:12:23 -0800

    tests were failing- fixed by removing tests/__init__

--- tests/test_saxskit.py
+++ tests/test_saxskit.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from saxskit import saxs_fit, saxs_classify
+from saxskit import saxs_math, saxs_fit, saxs_classify
 
 def test_guinier_porod():
     qvals = np.arange(0.01,1.,0.01)
-    Ivals = saxs_fit.guinier_porod(qvals,20,4,120)
+    Ivals = saxs_math.guinier_porod(qvals,20,4,120)
     assert isinstance(Ivals,np.ndarray)
 
 if __name__ == '__main__':
