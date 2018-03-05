799b890743604ddae71e570982b019128bb93626
Kate <ek.tcareva@gmail.com>
2018-03-05 14:21:22 -0800

    train and update_models scripts are updated

--- examples/update_models.py
+++ examples/update_models.py
@@ -4,10 +4,16 @@ warnings.filterwarnings("ignore")
 
 from citrination_client import CitrinationClient
 from saxskit.saxs_models import get_data_from_Citrination
-from saxskit.saxs_models import train_classifiers_partial, train_regressors_partial
+from saxskit.saxs_models import train_classifiers_partial, train_regressors_partial, save_models
 
-path = os.getcwd()
-api_key_file = path + '/citrination_api_key_ssrl.txt'
+p = os.path.abspath(__file__)
+d = os.path.dirname(os.path.dirname(p))
+classifiers_path = os.path.join(d,'saxskit','modeling_data','scalers_and_models.yml')
+regressors_path = os.path.join(d,'saxskit','modeling_data','scalers_and_models_regression.yml')
+
+api_key_file = os.path.join(d, 'api_key.txt')
+if not os.path.exists(api_key_file):
+    print("Citrination api key file did not find")
 
 with open(api_key_file, "r") as g:
     a_key = g.readline().strip()
@@ -17,14 +23,10 @@ new_data = get_data_from_Citrination(client = cl, dataset_id_list= [16]) # [16] 
 
 all_data = get_data_from_Citrination(client = cl, dataset_id_list= [1,15,16])
 
-train_classifiers_partial(new_data, yaml_filename = None, all_training_data = all_data)
+scalers, models, accuracy = train_classifiers_partial(
+        new_data, classifiers_path, all_training_data=all_data, model='all')
+save_models(scalers, models, accuracy, classifiers_path)
 
-with open(path + "/saxskit/saxskit/modeling_data/accuracy.txt", "r") as g:
-    accuracy = g.readline()
-print(accuracy)
-
-train_regressors_partial(new_data, yaml_filename = None, all_training_data = all_data)
-
-with open(path + "/saxskit/saxskit/modeling_data/accuracy_regression.txt", "r") as g:
-    accuracy = g.readline()
-print(accuracy)
+scalers, models, accuracy = train_regressors_partial(
+        new_data, regressors_path, all_training_data=all_data, model='all')
+save_models(scalers, models, accuracy, regressors_path)
