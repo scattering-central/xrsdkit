d4e70c48612a88c2c03c528aa1c83e916ce177f6
Lenson Pellouchoud <lensonp@gmail.com>
2018-03-16 11:42:29 -0700

    fixed some details of data model, added convenience constructors

--- xrsdkit/__init__.py
+++ xrsdkit/__init__.py
@@ -4,18 +4,23 @@ A scattering/diffraction pattern is assumed to represent
 one or more populations of scattering objects.
 A population is described by a dict with the following entries:
 
-    - 'lattice' : string indicating the lattice (e.g. 'fcc', 'dilute'). 
+    - 'name' : string population identifier 
+        (e.g. 'noise', 'substrate', 'particles')
 
-    - 'parameters' : dict describing the lattice parameters
+    - 'structure' : the structure of the population 
+        (e.g. 'fcc', 'diffuse', 'condensed'). 
+
+    - 'parameters' : dict describing the structure (lattice parameters, etc)
         as well as any other parameters used in the scattering computation.
-        Some of the keys are expected to refer to standard lattice notations:
+        Some of the keys are used for structural parameters:
 
         - 'a', 'b', 'c' : a, b, and c lattice parameters
         - 'alpha' : angle between b and c lattice vectors
         - 'beta' : angle between a and c lattice vectors
         - 'gamma' : angle between a and b lattice vectors
+        - 'N' : number of scatterers (for 'diffuse' structures)
     
-        Other keys are reserved for parameterizing diffraction peaks:
+        Other keys are used for parameterizing diffraction peaks:
 
         - 'q_min' : minimum q-value for reciprocal lattice points 
         - 'q_max' : maximum q-value for reciprocal lattice points 
@@ -29,46 +34,43 @@ A population is described by a dict with the following entries:
 
         - The occupancy is described by a dict containing  
             any number of form factor specifiers (as keys)
-            and corresponding form factor parameters (as values).
+            and form factor parameters (as values).
 
         - Each set of form factor parameters is a dict (or list of dicts)
             containing the parameter names (as keys) and values (as values).
-            If a list of dicts is given, 
-            the contributions from the scatterer populations 
-            described in the dicts will be summed in computing .
+            A list of dicts is used to include 
+            multiple scatterers of the same type.
 
-The following lattices are currently supported:
+The following structures are currently supported:
 
-    - 'dilute' : a dilute, non-interacting ensemble,
-        with no structure factor applied to the scattering.
-        This lattice has no parameters, no angles, 
+    - 'diffuse' : a diffuse (or dilute), 
+        non-interfering scattering ensemble.
+        This structure has one parameter, 
+        the number of scatterers 'N',
         and at least one basis site.
 
-    - 'fcc' : fcc lattice, with one parameter, no angles, 
+    - 'fcc' : fcc lattice, 
+        with one parameter, no angles, 
         and at least one basis site.
 
-
-The 'basis' of the lattice maps any number of fractional coordinates
-to a list of dicts describing the populations contributing to the form factor at that site.
-The supported populations and associated parameters are:
+The supported form factors and their parameters are:
 
     - 'flat': a flat form factor for all q
       
       - 'amplitude': amplitude of the flat scattering
 
     - 'guinier_porod': scatterer populations described 
-        by the Guinier-Porod equations
+        by the Guinier-Porod equations.
+        The square root of the Guinier-Porod intensity
+        is used as the form factor.
 
       - 'G': Guinier prefactor 
       - 'r_g': radius of gyration 
       - 'D': Porod exponent 
 
-    - 'spherical_normal': populations of spheres 
-        with normal (Gaussian) size distribution 
+    - 'spherical': solid spheres 
 
-      - 'sld_contrast': scattering length density contrast (Angstrom**2) 
-      - 'r0': mean sphere size (Angstrom) 
-      - 'sigma': fractional standard deviation of sphere size 
+      - 'r': sphere radius (Angstrom) 
 
     - 'atomic': atomic form factors described by
         ff = Z - 41.78214 * s**2 * sum_i(a_i*exp(-b_i*s**2)),
@@ -85,15 +87,21 @@ The supported populations and associated parameters are:
       - 'Z': atomic number
       - 'a': list of a coefficients 
       - 'b': list of b coefficients 
+
+    - all form factors:
+
+      - 'occupancy': occupancy fraction, used for sites 
+        with multiple fractional occupancies
+
         
 For example, a single Guinier-Porod scatterer 
-is placed in a 4-Angstrom fcc lattice,
+is placed in a 40-Angstrom fcc lattice,
 with peaks from q=0.1 to q=1.0 
-included in the structure factor:
+included in the summation:
 fcc_gp_population = dict(
-    lattice='fcc',
+    structure='fcc',
     parameters=dict(
-        a=4.0,
+        a=40.,
         q_min=0.1,
         q_max=1.,
         hwhm=0.01
@@ -118,26 +126,33 @@ import numpy as np
 
 from . import scattering, diffraction
 
-lattices = list([
-    'dilute',
+structures = list([
+    'diffuse',
     'fcc'])
+sf_parameters = OrderedDict(
+    peaks = list([
+        'hwhm',
+        'q_min',
+        'q_max',
+        'profile']),
+    diffuse = ['N'],
+    fcc = ['a'])
+
 form_factors = list([
     'flat',
     'guinier_porod',
     'spherical_normal',
     'atomic'])
-parameters = OrderedDict(
-    dilute = ['density'], 
+ff_parameters = OrderedDict(
+    general = ['occupancy'],
+    diffuse = ['density'], 
     fcc = ['a'],
     flat = ['amplitude'],
+    spherical = ['r'],
     guinier_porod = list([
         'G',
         'r_g',
         'D']),
-    spherical_normal = list([
-        'sld_contrast',
-        'r0',
-        'sigma']),
     atomic = list([
         'atom_name',
         'Z',
@@ -169,13 +184,37 @@ def compute_intensity(q,populations,source_wavelength):
     n_q = len(q)
     I = np.zeros(n_q)
     for popd in populations:
-        lat = popd['lattice']
-        if lat == 'dilute':
-            I += scattering.dilute_intensity(q,popd,source_wavelength)
+        lat = popd['structure']
+        if lat == 'diffuse':
+            I += scattering.diffuse_intensity(q,popd,source_wavelength)
         elif lat == 'fcc':
             I += diffraction.fcc_intensity(q,popd,source_wavelength)
         else:
-            msg = 'lattice specification {} is not supported'.format(lat)
+            msg = 'structure specification {} is not supported'.format(lat)
             raise ValueError(msg)
     return I
+
+def fcc_crystal(atom_name,a_lat,q_min=None,q_max=None,pk_profile=None,hwhm=None):
+    fcc_pop = dict(
+        name='fcc_{}'+atom_name,
+        structure='fcc',
+        parameters={'a':a_lat},
+        basis=dict((0,0,0)={'atomic':{'atom_name':atom_name}})
+        )
+    if q_min:
+        fcc_pop['parameters']['q_min'] = q_min
+    if q_max:
+        fcc_pop['parameters']['q_max'] = q_max
+    if pk_profile:
+        fcc_pop['parameters']['profile'] = pk_profile 
+    if hwhm:
+        fcc_pop['parameters']['hwhm'] = hwhm 
+    return fcc_pop
+
+# TODO: add more convenience constructors for various populations
+
+
+
+
+
 
