3fa9ffe8a8411d8eadfc041c0e8a3ba6342f8917
Lenson Pellouchoud <lensonp@gmail.com>
2018-03-23 10:11:14 -0700

    edits to structure_classifier and moved set_param helper function to models.__init__

--- xrsdkit/models/structure_classifier.py
+++ xrsdkit/models/structure_classifier.py
@@ -7,6 +7,7 @@ from sklearn import preprocessing,linear_model
 import yaml
 
 from .. import structure_names
+from . import set_param
 
 class StructureClassifier(object):
     """Models for classifying structure from scattering/diffraction data"""
@@ -29,7 +30,7 @@ class StructureClassifier(object):
 
         self.models = OrderedDict.fromkeys(structure_names)
         self.scalers = OrderedDict.fromkeys(structure_names)
-        self.accuracy = OrderedDict.fromkeys(structure_names)
+        self.cv_error = OrderedDict.fromkeys(structure_names)
         for struct_name in structure_names:
             model_params = classifier_dict[struct_name]
             scaler_params = scalers_dict[struct_name]
@@ -41,15 +42,7 @@ class StructureClassifier(object):
                 self.set_param(m,model_params)
             self.models[struct_name] = m
             self.scalers[struct_name] = s
-            self.accuracy[struct_name] = acc
-
-    # helper function - to set parametrs for scalers and models
-    def set_param(self, m_s, param):
-        for k, v in param.items():
-            if isinstance(v, list):
-                setattr(m_s, k, np.array(v))
-            else:
-                setattr(m_s, k, v)
+            self.cv_error[struct_name] = acc
 
     def classify(self, sample_features):
         """Determine the types of structures represented by the sample
@@ -95,15 +88,20 @@ class StructureClassifier(object):
 
         return populations, certainties
 
-    def get_accuracy(self):
-        """Get accuracy for all classification models.
+    def training_cv_error(self):
+        """Report cross-validation error for these classification models.
+
+        "Leave-2-Groups-Out" cross-validation is used.
+        For each train-test split, 
+        two experiments are used for testing 
+        and the rest are used for training. 
+        The reported error is the average over all train-test splits. 
+        TODO: what is the error metric for the classifier? 
+        TODO: verify that this docstring is correct
 
         Returns
         -------
-        accuracy : dict
-            Dictionary of models and their accuracies.
-            to calculate the accuracy "Leave-N-Groups-Out" technique is used.
-            Every cycle data from two experiments used for testing and the
-            other data for training. The average accuracy is reported.
+        cv_errors : dict
+            Dictionary of models and their cross-validation errors. 
         """
-        return self.accuracy
+        return self.cv_error
