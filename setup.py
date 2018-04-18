97fb6ec815fc98033a69339df16b010a63a290a1
Lenson Pellouchoud <lensonp@gmail.com>
2018-04-18 10:04:25 -0700

    updated setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='xrsdkit',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/scattering-central/xrsdkit.git',
     description='Scattering and diffraction analysis and modeling toolkit',
     author='SSRL',
     author_email='paws-developers@slac.stanford.edu',
     packages=find_packages(),
     install_requires=[
-        'numpy','scipy','scikit-learn'
+        'pyyaml','numpy','scipy','pandas','scikit-learn','lmfit'
     ],
 )
