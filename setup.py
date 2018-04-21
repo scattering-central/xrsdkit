5cdfc8221153cc16ecd2e75d8dcc71d854d51e3a
Lenson Pellouchoud <lensonp@gmail.com>
2018-04-21 07:25:54 -0700

    adding yml data to setup.py, version +

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='xrsdkit',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/scattering-central/xrsdkit.git',
     description='Scattering and diffraction analysis and modeling toolkit',
     author='SSRL',
@@ -10,4 +10,6 @@ setup(name='xrsdkit',
     install_requires=[
         'pyyaml','numpy','scipy','pandas','scikit-learn','lmfit','pymatgen'
     ],
+    include_package_data=True,
+    package_data={'xrsdkit':['scattering/atomic_scattering_params.yml','models/modeling_data/*.yml']}
 )
