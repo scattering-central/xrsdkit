a6bf808dc0525aa49b88f42e2d45fd4097936c2b
Kate <ek.tcareva@gmail.com>
2018-06-20 17:00:34 -0700

    name for sample dataset was updated

--- xrsdkit/tools/citrination_tools.py
+++ xrsdkit/tools/citrination_tools.py
@@ -1,12 +1,15 @@
 from collections import OrderedDict
-
+import os
 import pandas as pd
 import numpy as np
-from citrination_client import CitrinationClient
+from sklearn import preprocessing
+from pypif import pif
 from citrination_client import PifSystemReturningQuery, DatasetQuery, DataQuery, Filter
 
 from ..tools import profiler
 from ..tools import piftools
+#import paws
+#from pypaws.operations.IO.CITRINATION.UploadPIF import UploadPIF
 
 
 def get_data_from_Citrination(client, dataset_id_list):
@@ -86,4 +89,109 @@ def get_pifs_from_Citrination(client, dataset_id_list):
     pifs = [x.system for x in all_hits]
     return pifs
 
+def sampl_data_on_Citrination(client, data_cl, dataset_id_list):
+    """Create a sample of data and ship it on Citrination.
 
+    Parameters
+    ----------
+    client : citrination_client.CitrinationClient
+        A python Citrination client for fetching data
+    data_cl : citrination_client.data.client.DataClient
+        A python Citrination client encapsulating data management behavior.
+    dataset_id_list : list of int
+        List of dataset ids (integers) for fetching SAXS records
+
+    Returns
+    -------
+    new_datase_id : int
+        id of new dataset
+    """
+    my_list = ','.join(map(str, dataset_id_list))
+    sample_dataset_name = "sample_from_"+my_list
+    ds = data_cl.create_dataset(sample_dataset_name, "Test sampling of data") # TODO name for new dataset sould be unique
+    new_datase_id = ds.id
+
+    p = os.path.abspath(__file__)
+    d = os.path.dirname(os.path.dirname(os.path.dirname(p)))
+
+    data = []
+    pifs = get_pifs_from_Citrination(client,dataset_id_list)
+
+    for i in range(len(pifs)):
+        pp = pifs[i]
+        expt_id,_,_,_,pp_feats, cl_model_outputs, _ = piftools.unpack_pif(pp)
+        feats = OrderedDict.fromkeys(profiler.profile_keys)
+        feats.update(pp_feats)
+
+        data_row = [expt_id]+list(feats.values())+[cl_model_outputs] + [i] # i will be the local_id
+        data.append(data_row)
+
+    colnames = ['experiment_id']
+    colnames.extend(profiler.profile_keys)
+    colnames.extend(['system_class'])
+    colnames.extend(['local_id'])
+
+    df = pd.DataFrame(data=data, columns=colnames)
+    data = df.where((pd.notnull(df)), None) # replace all NaN by None
+
+    data_sample = pd.DataFrame(columns=data.columns)
+    all_exp = data.experiment_id.unique()
+
+    features = []
+    features.extend(profiler.profile_keys_1)
+
+    scaler = preprocessing.StandardScaler()
+    scaler.fit(data[features])
+    data[features] = scaler.transform(data[features])
+
+    for exp_id in all_exp:
+        df = data[data['experiment_id']==exp_id]
+        sample = make_sample_one_experiment(df, 1.0)
+        data_sample = data_sample.append(sample)
+
+    #print(data_sample.shape)
+    count = data_sample.shape[0]
+
+    samples_to_save = data_sample.local_id.tolist()
+
+    pifs_to_save = []
+    for samp_id in samples_to_save:
+        pifs_to_save.append(pifs[samp_id])
+
+    pif_file = os.path.join(d, 'test.json')
+
+    pif.dump(pifs_to_save, open(pif_file,'w'))
+    r = client.data.upload(new_datase_id, pif_file)
+
+    return new_datase_id, count
+
+
+def make_sample_one_experiment(data_fr, min_distance):
+    """make a sample from ONE experiment.
+    Parameters
+    ----------
+    data_fr : pandas.DataFrame
+        dataframe containing the samples from one experiment.
+    min_distance : float
+        the minimal allowed distance between the samples.
+    Returns
+    -------
+    sample : pandas.DataFrame
+        dataframe containing subset of rows
+        that was chosen using distance between the samples
+    """
+    groups_by_class = data_fr.groupby('system_class')
+    sample = pd.DataFrame(columns=data_fr.columns)
+    for name, group in groups_by_class:
+        df = pd.DataFrame(columns=data_fr.columns)
+        df = df.append(group.iloc[0])
+        for i in range(1, group.shape[0]):
+            add_row = True
+            for j in range(0, df.shape[0]):
+                s = sum((group.iloc[i][profiler.profile_keys_1] - group.iloc[j][profiler.profile_keys_1]).abs())
+                if s < min_distance:
+                    add_row = False
+            if add_row:
+                df = df.append(group.iloc[i])
+        sample = sample.append(df)
+    return sample
