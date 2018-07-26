2e630c5336f87add31e93991eb15aee8760e0b13
Lenson Pellouchoud <lensonp@gmail.com>
2018-07-25 18:12:26 -0700

    renamed structure_classifier.py to classifer.py, refactored regressor.py and classifier.py

---
+++ xrsdkit/models/classifier.py
@@ -0,0 +1,132 @@
+import numpy as np
+from sklearn import linear_model
+
+from .xrsd_model import XRSDModel
+
+class SystemClassifier(Classifier):
+    """Classifier for determining the material system (structures and form factors).
+
+    See the main xrsdkit package documentation 
+    for all supported structures and parameters
+    for defining the material system.
+    """
+
+    def __init__(self,yml_file):
+        super(SystemClassifier,self).__init__('system_class', yml_file)
+
+
+class Classifier(XRSDModel):
+    """Class for generating models to classifying material systems."""
+
+    def __init__(self,label,yml_file):
+        super(Classifier,self).__init__(label, yml_file)
+        self.grid_search_hyperparameters = dict(
+            penalty = ['none', 'l2', 'l1', 'elasticnet'], # default: l2
+            alpha = [0.00001, 0.0001, 0.001, 0.01, 0.1], # regularisation coef, default 0.0001
+            l1_ratio = [0, 0.15, 0.5, 0.85, 1.0] # default 0.15, only valid for elasticnet penalty
+            )
+
+    def build_model(self,model_hyperparams={}):
+        if all([p in model_hyperparams for p in ['alpha','penalty','l1_ratio']]):
+            new_model = linear_model.SGDClassifier(
+                    alpha=model_hyperparams['alpha'], 
+                    loss='log',
+                    penalty=model_hyperparams['penalty'], 
+                    l1_ratio=model_hyperparams['l1_ratio'],
+                    max_iter=10)
+        else:
+            new_model = linear_model.SGDClassifier(loss='log', max_iter=10)
+        return new_model
+
+    def classify(self, sample_features):
+        """Classify the model target for a sample.
+
+        Parameters
+        ----------
+        sample_features : OrderedDict
+            OrderedDict of features with their values,
+            similar to output of xrsdkit.tools.profiler.profile_spectrum()
+
+        Returns
+        -------
+        structure_flags : bool or None
+            a boolean inidicating whether or not
+            the sample exhibits the structure
+            None is reterned for models that was not trained yet
+        cert : float or None
+            the certainty of the prediction
+            None is reterned for models that was not trained yet
+        """
+        feature_array = np.array(list(sample_features.values())).reshape(1,-1)
+        x = self.scaler.transform(feature_array)
+        sys_cls = self.model.predict(x)[0]
+        cert = max(self.model.predict_proba(x)[0])
+        return sys_cls, cert
+
+    def run_cross_validation(self,model,data,group_cv):
+        if group_cv:
+            new_accuracy = self.cross_validate_by_experiments(model,data)
+        else:
+            new_accuracy = self.cross_validate(model,data)
+        return new_accuracy
+
+    def cross_validate(self, model, df):
+        """Test a model using scikit-learn 5-fold crossvalidation
+
+        Parameters
+        ----------
+        model : sklearn model
+            model to be cross-validated 
+        df : pandas.DataFrame
+            pandas dataframe of features and labels
+
+        Returns
+        -------
+        scores : object
+            TODO: describe scores output
+        """
+        scaler = preprocessing.StandardScaler()
+        scaler.fit(df[profiler.profile_keys])
+        scores = model_selection.cross_val_score(
+            model, scaler.transform(df[profiler.profile_keys]), 
+            df[self.target], cv=5)
+        return scores
+
+    def cross_validate_by_experiments(self, model, df):
+        """Test a model by LeaveOneGroupOut cross-validation.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            pandas dataframe of features and labels
+        model : sk-learn
+            with specific parameters
+
+        Returns
+        -------
+        test_scores_by_ex : object
+            TODO: describe scores output
+        """
+        experiments = df.experiment_id.unique()# we have at least 5 experiments
+        test_scores_by_ex = []
+        for i in range(len(experiments)):
+            tr = df[(df['experiment_id'] != experiments[i])]
+            test = df[(df['experiment_id'] == experiments[i])]
+            # for testing, we want only the samples with labels that are
+            # included in training set:
+
+            # TODO: not clear why this is needed- consider removing?
+            # (all samples in the dataset should have some label for the target)
+            tr_labels = tr[self.target].unique()
+            test = test[test[self.target].isin(tr_labels)]
+
+            scaler = preprocessing.StandardScaler()
+            scaler.fit(tr[profiler.profile_keys])
+            model.fit(scaler.transform(tr[profiler.profile_keys]), tr[self.target])
+            transformed_data = scaler.transform(test[profiler.profile_keys])
+            test_score = model.score(
+                transformed_data, test[self.target])
+            test_scores_by_ex.append(test_score)
+
+        return test_scores_by_ex
+
--- xrsdkit/models/regressor.py
+++ xrsdkit/models/regressor.py
@@ -1,29 +1,39 @@
 import numpy as np
-from .general_model import XRSDModel
-from ..tools import profiler
-from xrsdkit.tools.profiler import guinier_porod_profile, spherical_normal_profile
+from sklearn import linear_model
+
+from .xrsd_model import XRSDModel
+from ..tools.profiler import guinier_porod_profile, spherical_normal_profile
 
 class Regressor(XRSDModel):
-    """To create regressor for prediction continuous value from scattering/diffraction data;
-    train, update, and save it; make a prediction."""
+    """Class for generating models to predict real-valued parameters."""
 
-    def __init__(self,label, system_class, yml_file=None):
+    def __init__(self,label,yml_file):
+        super(Regressor,self).__init__(label, yml_file)
+        self.grid_search_hyperparameters = dict( 
+            loss = ['huber', 'squared_loss'], # huber with epsilon = 0 yields mean abs error (MAE)
+            epsilon = [1, 0.1, 0.01, 0.001, 0],  
+            penalty = ['none', 'l2', 'l1', 'elasticnet'], # default: l2  
+            alpha = [0.0001, 0.001, 0.01], # regularisation coef, default 0.0001 
+            l1_ratio = [0, 0.15, 0.5, 0.95] # default 0.15, only valid for elasticnet penalty 
+            )
 
-        super(Regressor,self).__init__(label, system_class, yml_file, False)
-
-        self.n_groups_out = 1
-
-        f = []
-        f.extend(profiler.profile_keys_1)
-        if 'rg' in label:
-            f.extend(profiler.gp_profile_keys)
-        elif 'r0' or 'sigma' in label:
-            f.extend(profiler.spherical_profile_keys)
-        self.features = f
-
+    def build_model(self, model_hyperparams={}):
+        if all([p in model_hyperparams for p in ['alpha','loss','penalty','l1_ratio','epsilon']]):
+            new_model = linear_model.SGDRegressor(
+                    alpha=model_hyperparams['alpha'], 
+                    loss=model_hyperparams['loss'],
+                    penalty=model_hyperparams['penalty'],
+                    l1_ratio=model_hyperparams['l1_ratio'],
+                    epsilon=model_hyperparams['epsilon'],
+                    max_iter=1000)
+        else:
+            # NOTE: max_iter is about 10^6 / number of tr samples 
+            new_model = linear_model.SGDRegressor(max_iter=1000) 
+        return new_model
 
     def predict(self, sample_features, q_I):
-        """Determine the types of structures represented by the sample
+        """Predict this model's scalar target for a given sample. 
+
         Parameters
         ----------
         sample_features : OrderedDict
@@ -31,26 +41,87 @@ class Regressor(XRSDModel):
             similar to output of xrsdkit.tools.profiler.profile_spectrum()
         q_I : array
             n-by-2 array of scattering vector (1/Angstrom) and intensities.
+
         Returns
         -------
-        prediction : float or None
-            predicted parameter
-            None is reterned for models that was not trained yet
+        prediction : float
+            predicted parameter value
         """
         if 'rg' in self.target:
             additional_features = guinier_porod_profile(q_I)
             my_features = np.append(np.array(list(sample_features.values())),
                                     np.array(list(additional_features.values()))).reshape(1,-1)
 
-        elif 'r0' or 'sigma' in self.target:
+        elif 'r0' in self.target or 'sigma' in self.target:
             additional_features = spherical_normal_profile(q_I)
             my_features = np.append(np.array(list(sample_features.values())),
                                     np.array(list(additional_features.values()))).reshape(1,-1)
 
-        if self.scaler: # we have a saved model
-            x = self.scaler.transform(my_features)
-            prediction = float(self.model.predict(x)[0])
+        x = self.scaler.transform(my_features)
+        return float(self.model.predict(x)[0])
 
-            return prediction
+    def run_cross_validation(self,model,data,group_cv):
+        label_std = pd.to_numeric(data[self.target]).std()
+        if group_cv:
+            new_accuracy = self.cross_validate_by_experiments(model,data,label_std)
         else:
-            return None
+            new_accuracy = self.cross_validate(model,data,label_std)
+        return new_accuracy
+
+    def cross_validate(self,model,df,label_std):
+        """Test a model using scikit-learn 5-fold crossvalidation
+
+        Parameters
+        ----------
+        model : sklearn model
+            model to be cross-validated 
+        df : pandas.DataFrame
+            pandas dataframe of features and labels
+        label_std : float
+            standard deviation of training data for the model label 
+
+        Returns
+        -------
+        scores : object
+            TODO: describe scores output
+        """
+        scaler = preprocessing.StandardScaler()
+        scaler.fit(df[profiler.profile_keys])
+        scores = model_selection.cross_val_score(
+                model,scaler.transform(df[profiler.profile_keys]), df[self.target],
+                cv=5, scoring='neg_mean_absolute_error')
+        return scores 
+
+    def cross_validate_by_experiments(self, model, df, label_std):
+        """Test a model by LeaveOneGroupOut cross-validation.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            pandas dataframe of features and labels
+        model : sk-learn
+            with specific parameters
+        label_std : float
+            standard deviation of training data for the model label 
+            
+        Returns
+        -------
+        test_scores_by_ex : object
+            TODO: describe scores output
+        """
+        experiments = df.experiment_id.unique()
+        test_scores_by_ex = []
+        for i in range(len(experiments)):
+            tr = df[(df['experiment_id']!= experiments[i])]
+            test = df[(df['experiment_id']== experiments[i])]
+
+            scaler = preprocessing.StandardScaler()
+            scaler.fit(tr[profiler.profile_keys])
+            model.fit(scaler.transform(tr[profiler.profile_keys]), tr[self.target])
+            transformed_data = scaler.transform(test[profiler.profile_keys])
+            pr = model.predict(transformed_data)
+            test_score = mean_absolute_error(pr, test[self.target])
+            test_scores_by_ex.append(test_score/label_std)
+
+        return test_scores_by_ex
+
