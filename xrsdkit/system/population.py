9cc99ffd3214c9ab8fb6820ebb03f77616f832e2
Lenson Pellouchoud <lensonp@gmail.com>
2018-08-08 16:37:28 -0700

    bugfixes in xrsdkit.system classes, removed xrsdkit.system.parameter

--- xrsdkit/system/population.py
+++ xrsdkit/system/population.py
@@ -1,7 +1,8 @@
+import copy
 from collections import OrderedDict
 
 from .specie import Specie
-from .parameter import Parameter
+from .. import * 
 
 from ..scattering import diffuse_intensity, disordered_intensity, crystalline_intensity
 
@@ -9,6 +10,10 @@ class Population(object):
 
     def __init__(self,structure,settings={},parameters={},basis={}):
         # TODO: validate basis entries?
+        self.structure = None
+        self.settings = {}
+        self.parameters = {}
+        self.basis = {}
         self.set_structure(structure)
         self.update_basis(basis)
         self.update_settings(settings)
@@ -16,6 +21,7 @@ class Population(object):
 
     def set_structure(self,structure):
         self.check_structure(structure,self.basis_to_dict())
+        # first update settings
         new_settings = dict.fromkeys(structure_settings[structure])
         for stg_nm in structure_settings[structure]:
             if stg_nm in self.settings:
@@ -23,21 +29,26 @@ class Population(object):
             else:
                 new_settings[stg_nm] = setting_defaults[stg_nm]
         self.settings = new_settings
+        # second update self.parameters, wrt self.settings
         new_params = dict.fromkeys(structure_params[structure])  
-        for param_nm in structure_params[structure]):
+        if structure == 'disordered':
+            new_params.update(dict.fromkeys(
+            disordered_structure_params[self.settings['interaction']]))
+        if structure == 'crystalline':
+            new_params.update(dict.fromkeys(
+            crystalline_structure_params[self.settings['lattice']]))
+        for param_nm in new_params.keys():
             if param_nm in self.parameters:
                 new_params[param_nm] = self.parameters[param_nm]
             else:
-                new_params[param_nm] = Parameter.from_dict(param_defaults[param_nm])
+                new_params[param_nm] = copy.deepcopy(param_defaults[param_nm])
         self.parameters = new_params
+        self.structure = structure
 
     def add_specie(self,specie_name,ff_name,settings={},parameters={},coordinates=None):
         if self.structure in crystalline_structures and ff_name in noncrystalline_form_factors:
             structure_form_exception(self.structure,ff_name)
-        self.basis[specie_name] = Specie(ff_name)
-        self.basis[specie_name].update_settings(settings)
-        self.basis[specie_name].update_parameters(parameters)
-        self.basis[specie_name].update_coordinates(coordinates)
+        self.basis[specie_name] = Specie(ff_name,settings,parameters,coordinates)
 
     def to_dict(self):
         pd = {} 
@@ -47,7 +58,7 @@ class Population(object):
             pd['settings'][stg_nm] = stg 
         pd['parameters'] = {}
         for param_nm,param in self.parameters.items():
-            pd['parameters'][param_nm] = param.to_dict()
+            pd['parameters'][param_nm] = copy.deepcopy(param)
         pd['basis'] = self.basis_to_dict()
         return pd
 
@@ -66,7 +77,7 @@ class Population(object):
     def basis_to_dict(self):
         bd = {}
         for specie_nm,specie in self.basis.items():
-            pd['basis'][specie_nm] = specie.to_dict()
+            bd[specie_nm] = specie.to_dict()
         return bd
 
     def basis_to_ordered_dict(self):
@@ -91,21 +102,21 @@ class Population(object):
     def update_parameters(self,pd):
         for param_nm, paramd in pd.items():
             if param_nm in self.parameters:
-                self.parameters[param_nm].update_from_dict(paramd)
+                self.parameters[param_nm].update(paramd)
 
-    def update_settings(self,sd)
+    def update_settings(self,sd):
         for stg_nm, sval in sd.items():
             if stg_nm in self.settings:
                 self.settings[stg_nm] = sval
 
     def update_basis(self,bd):
         self.check_structure(self.structure,bd)
-            for specie_nm,specd in bd.items():
-                self.basis[specie_nm] = Specie.from_dict(specd)
+        for specie_nm,specd in bd.items():
+            self.basis[specie_nm] = Specie.from_dict(specd)
 
     @classmethod
     def from_dict(cls,d):
-        inst = cls()
+        inst = cls(d['structure'])
         inst.update_from_dict(d)
         return inst
 
--- xrsdkit/system/specie.py
+++ xrsdkit/system/specie.py
@@ -1,13 +1,18 @@
-from .parameter import Parameter
+import copy
+
+from .. import *
 
 class Specie(object):
 
-    def __init__(self,form,settings={},parameters={},coordinates=None):
+    def __init__(self,form,settings={},parameters={},coordinates=[]):
+        self.form = None
+        self.settings = {}
+        self.parameters = {}
+        make_coord = lambda : {'value':0.,'fixed':True,'bounds':[-1.,1.],'constraint_expr':None}  
+        self.coordinates = [make_coord(),make_coord(),make_coord()]
         self.set_form(form)
         self.update_settings(settings)
         self.update_parameters(parameters)
-        make_coord = lambda : Parameter(0.,True,[None,None],None)  
-        self.coordinates = [make_coord(),make_coord(),make_coord()]
         self.update_coordinates(coordinates)
 
     def set_form(self,form):
@@ -19,12 +24,13 @@ class Specie(object):
                 new_settings[stg_nm] = setting_defaults[stg_nm]
         self.settings = new_settings 
         new_params = dict.fromkeys(form_factor_params[form])
-        for param_nm in form_factor_params[form]):
+        for param_nm in form_factor_params[form]:
             if param_nm in self.parameters:
                 new_params[param_nm] = self.parameters[param_nm]
             else:
-                new_params[param_nm] = Parameter.from_dict(param_defaults[param_nm])
+                new_params[param_nm] = copy.deepcopy((param_defaults[param_nm]))
         self.parameters = new_params
+        self.form = form
 
     def to_dict(self):
         sd = {}
@@ -34,11 +40,17 @@ class Specie(object):
             sd['settings'][stg_nm] = stg 
         sd['parameters'] = {}
         for param_nm,param in self.parameters.items():
-            sd['parameters'][param_nm] = param.to_dict()
+            sd['parameters'][param_nm] = copy.deepcopy(param)
         sd['coordinates'] = [None,None,None] 
         for ic,cparam in enumerate(self.coordinates):
-            sd['coordinates'][ic] = cparam.to_dict()
+            sd['coordinates'][ic] = copy.deepcopy(cparam)
         return sd
+
+    @classmethod
+    def from_dict(cls,d):
+        inst = cls(d['form'])
+        inst.update_from_dict(d)
+        return inst
 
     def update_from_dict(self,d):
         if 'form' in d:
@@ -55,12 +67,12 @@ class Specie(object):
             if param_nm in self.parameters:
                 self.parameters[param_nm].update_from_dict(paramd)
 
-    def update_settings(self,sd)
+    def update_settings(self,sd):
         for stg_nm, sval in sd.items():
             if stg_nm in self.settings:
                 self.settings[stg_nm] = sval
 
-    def update_coordinates(self,coords)
+    def update_coordinates(self,coords):
         for ic, cdict in enumerate(coords):
             if cdict is not None:
                 self.coordinates[ic].update_from_dict(cdict)
