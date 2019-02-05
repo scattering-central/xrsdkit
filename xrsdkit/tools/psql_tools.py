5359071e609921d6672811ce0816578099cbc6cc
Kate <ek.tcareva@gmail.com>
2019-02-05 09:14:32 -0800

    create_training_table works

--- xrsdkit/tools/psql_tools.py
+++ xrsdkit/tools/psql_tools.py
@@ -133,6 +133,82 @@ def load_from_files_table_to_samples_table(db, ssh_client, drop_table=False):
                                   features=features, regression_labels=regression_labels,
                                     classification_labels=classification_labels)
 
+
+def create_training_table(db, drop_table=False):
+    """Process the data from a the "samples" table and
+    add to the "training" table in the format sutable for training
+    (each feature and labels has its own column).
+    Only data from new experiment directories will be added.
+
+    Parameters
+    ----------
+    db : db.pg object
+        a database connection - DB object from PyGreSQL
+    drop_table : bool
+        if True, the existing table will be dropped and a new table will be created
+        from scratch;
+        if False, only data that is not already in the "samples" table will be added.
+    """
+    if drop_table:
+            db.query("DROP TABLE training")
+
+    #db.query("DROP TABLE training")
+    # get all existing classification labes:
+    q = 'SELECT json_object_keys(classification_labels) FROM samples'
+    all_cl_labels = set([r[0] for r in db.query(q).getresult()])
+
+    # get all existing regression labels:
+    q = 'SELECT json_object_keys(regression_labels) FROM samples'
+    all_reg_labels = set([r[0] for r in db.query(q).getresult()])
+
+    # notes: this table will be used for creating training dataframe;
+    # the columns of this table must have the exactly the same formatting
+    # as profiler.profiler_keys including low/upper cases
+    q = 'CREATE TABLE IF NOT EXISTS training(sample_id VARCHAR PRIMARY KEY, experiment_id VARCHAR, "' + \
+        '" NUMERIC, "'.join(profile_keys) + '" NUMERIC)'
+    db.query(q)
+
+    #add new columns if it is needed (new classification and regression labels)
+    q = 'ALTER TABLE training ADD COLUMN IF NOT EXISTS "' + \
+        '" VARCHAR, ADD COLUMN IF NOT EXISTS "'.join(all_cl_labels) + '" VARCHAR, ADD COLUMN IF NOT EXISTS "' + \
+        '" NUMERIC, ADD COLUMN IF NOT EXISTS "'.join(all_reg_labels) + '" NUMERIC'
+    db.query(q)
+
+    # get the list of experiments that are not in the 'training' table
+    new_experiments = db.query("SELECT DISTINCT experiment_id "
+                               " FROM samples "
+                               " WHERE experiment_id NOT IN ("
+                                    "SELECT DISTINCT experiment_id "
+                                    "FROM training "
+                                    "WHERE experiment_id IS NOT NULL)").getresult()
+    new_experiments = [row[0] for row in new_experiments]
+
+    # get data from the "samples" table from the experiments that are not in "training" table:
+    for ex in new_experiments:
+        print('reading data from {}'.format(ex))
+        q = "SELECT * FROM samples WHERE experiment_id = '{}'".format(ex)
+        experiment_data = db.query(q).dictresult() # list of dict
+        print('done - found {} records'.format(len(experiment_data)))
+
+        for f in experiment_data:
+            features = OrderedDict(f['features'])
+            cl_labels = OrderedDict(f['classification_labels'])
+            reg_labels = OrderedDict(f['regression_labels'])
+            # add a new row to the table "samples"
+            feature_values_as_str = [str(v) for v in features.values()]
+            reg_values_as_str = [str(v) for v in reg_labels.values()]
+
+            # the next query: 'INSERT INTO training (sample_id, experiment_id, "Feature_1", "Feature_2", ...
+            # , "Classification_label_1", .... , "Regression_label_1") VALUES ('R1_000_34', 'R1', '0.7855869',...'
+            # We need to use double quotes for feature names since we want to preserve the letter cases.
+            q_row = 'INSERT INTO training (sample_id, experiment_id, "' + '", "'.join(features.keys()) +\
+                    '", "'+'", "'.join(cl_labels.keys()) +'", "'+ '", "'.join(reg_labels.keys()) + \
+                    '") VALUES (' +"'"+ f['sample_id'] +"'" + ", " + "'"+  f['experiment_id'] +"'" + ", " + \
+                    ', '.join(feature_values_as_str) +", '"+ "', '".join(f['classification_labels'].values()) + \
+                    "', "+', '.join(reg_values_as_str) + ")"
+            db.query(q_row)
+
+
 '''
 
 def load_from_yml_to_file_table(db, path_to_dir):
