8bd753fbc5a4ee23fdc1afab45e12357fb6688ef
Lenson Pellouchoud <lensonp@gmail.com>
2019-02-08 15:21:40 -0800

    version+

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='xrsdkit',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/scattering-central/xrsdkit.git',
     description='Scattering and diffraction analysis and modeling toolkit',
     author='SSRL',
