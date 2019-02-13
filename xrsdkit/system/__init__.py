92f85e27b6ae3a338363bd448d6eabf82c01733d
Lenson <lenson@slac.stanford.edu>
2019-02-12 17:37:58 -0800

    default values for fitting settings

--- xrsdkit/system/__init__.py
+++ xrsdkit/system/__init__.py
@@ -22,13 +22,20 @@ class System(object):
 
     def __init__(self,**kwargs):
         self.populations = {}
-        self.fit_report = {'good_fit':False}
+        self.fit_report = dict(
+            error_weighted=True,
+            logI_weighted=True,
+            good_fit=False,
+            q_range=[0.,float('inf')]
+            )
         self.features = {}
+        src_wl = 0.
+        if 'source_wavelength' in kwargs: src_wl = kwargs['source_wavelength'] 
         self.sample_metadata = dict(
             experiment_id='',
             sample_id='',
             data_file='',
-            source_wavelength=0.,
+            source_wavelength=src_wl,
             time=0.,
             notes=''
             )
@@ -78,6 +85,15 @@ class System(object):
                     for param_name, paramd in popd['parameters'].items():
                         self.populations[pop_name].parameters[param_name].update(popd['parameters'][param_name])
 
+    def set_q_range(self,q_min,q_max):
+        self.fit_report['q_range'] = [float(q_min),float(q_max)]
+
+    def set_error_weighted(self,err_wtd):
+        self.fit_report['error_weighted'] = bool(err_wtd) 
+
+    def set_logI_weighted(self,logI_wtd):
+        self.fit_report['logI_weighted'] = bool(logI_wtd) 
+
     def remove_population(self,pop_nm):
         # TODO: check for violated constraints
         # in absence of this population
@@ -112,8 +128,7 @@ class System(object):
             I += pop.compute_intensity(q,self.sample_metadata['source_wavelength'])
         return I
 
-    def evaluate_residual(self,q,I,dI=None,
-        error_weighted=True,logI_weighted=True,q_range=[0.,float('inf')],I_comp=None):
+    def evaluate_residual(self,q,I,dI=None,I_comp=None):
         """Evaluate the fit residual for a given populations dict.
     
         Parameters
@@ -140,18 +155,19 @@ class System(object):
         res : float
             Value of the residual 
         """
+        q_range = self.fit_report['q_range']
         if I_comp is None:
             I_comp = self.compute_intensity(q)
         idx_nz = (I>0)
         idx_fit = (idx_nz) & (q>=q_range[0]) & (q<=q_range[1])
         wts = np.ones(len(q))
-        if error_weighted:
+        if self.fit_report['error_weighted']:
             if dI is None:
                 dI = np.empty(I.shape)
                 dI.fill(np.nan)
                 dI[idx_fit] = np.sqrt(I[idx_fit])
             wts *= dI**2
-        if logI_weighted:
+        if self.fit_report['logI_weighted']:
             idx_fit = idx_fit & (I_comp>0)
             # NOTE: returning float('inf') raises a NaN exception within the minimization.
             #if not any(idx_fit):
@@ -166,14 +182,14 @@ class System(object):
                 I[idx_fit],
                 wts[idx_fit])
         return res 
-    
-    def lmf_evaluate(self,lmf_params,q,I,dI=None,error_weighted=True,logI_weighted=True,q_range=[None,None]):
+
+    def lmf_evaluate(self,lmf_params,q,I,dI=None):
         new_params = unpack_lmfit_params(lmf_params)
         old_params = self.flatten_params()
         old_params.update(new_params)
         new_pd = unflatten_params(old_params)
         self.update_params_from_dict(new_pd)
-        return self.evaluate_residual(q,I,dI,error_weighted,logI_weighted,q_range)
+        return self.evaluate_residual(q,I,dI)
 
     def pack_lmfit_params(self):
         p = self.flatten_params() 
@@ -230,16 +246,15 @@ def fit(sys,q,I,dI=None,
     # the System to optimize starts as a copy of the input System
     sys_opt = System.from_dict(sys.to_dict())
 
-    obj_init = sys_opt.evaluate_residual(q,I,dI,error_weighted,logI_weighted,q_range)
+    obj_init = sys_opt.evaluate_residual(q,I,dI)
     lmf_params = sys_opt.pack_lmfit_params() 
-    lmf_res = lmfit.minimize(sys_opt.lmf_evaluate,
+    lmf_res = lmfit.minimize(
+        sys_opt.lmf_evaluate,
         lmf_params,method='nelder-mead',
-        kws={'q':q,'I':I,'dI':dI,
-            'error_weighted':error_weighted,
-            'logI_weighted':logI_weighted,
-            'q_range':q_range})
+        kws={'q':q,'I':I,'dI':dI}
+        )
 
-    fit_obj = sys_opt.evaluate_residual(q,I,dI,error_weighted,logI_weighted,q_range)
+    fit_obj = sys_opt.evaluate_residual(q,I,dI)
     I_opt = sys_opt.compute_intensity(q)
     I_bg = I - I_opt
     snr = np.mean(I_opt)/np.std(I_bg) 
