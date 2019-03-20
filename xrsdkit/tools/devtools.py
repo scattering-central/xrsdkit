554cd0d0ef8a9608a4ed97ae1a60e72888a18aa6
Lenson Pellouchoud <lensonp@gmail.com>
2019-03-20 11:17:06 -0700

    added feature selection arguments to devtools training functions

--- xrsdkit/tools/devtools.py
+++ xrsdkit/tools/devtools.py
@@ -6,11 +6,11 @@ from ..models.train import train_from_dataframe
 
 def train_models_from_local_dataset(dataset_dir,downsampling_distance=1.):
     df = read_local_dataset(dataset_dir,downsampling_distance=downsampling_distance) 
-    train_from_dataframe(df,train_hyperparameters=True,save_models=True,test=False)
+    train_from_dataframe(df,train_hyperparameters=True,select_features=True,save_models=True,test=False)
 
 def train_models_from_remote_dataset(dataset_dir,downsampling_distance=1.):
     df = gather_remote_dataset(dataset_dir,downsampling_distance=downsampling_distance)
-    train_from_dataframe(df,train_hyperparameters=True,save_models=True,test=False)
+    train_from_dataframe(df,train_hyperparameters=True,select_features=True,save_models=True,test=False)
 
 def dataset_to_csv(dataset_dir,downsampling_distance=1.):
     df = read_local_dataset(dataset_dir,downsampling_distance=downsampling_distance) 
