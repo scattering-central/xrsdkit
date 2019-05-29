df4c386658aff3df876c94001119981d468ff2c3
Lenson <lenson@slac.stanford.edu>
2019-05-29 16:16:10 -0700

    fixed handling of error_weighted and logI_weighted flags

--- xrsdkit/system/__init__.py
+++ xrsdkit/system/__init__.py
@@ -126,6 +126,8 @@ class System(object):
             I += pop.compute_intensity(q,self.sample_metadata['source_wavelength'])
         return I
 
+    # TODO: take logI_weighted and error_weighted as optional inputs.
+    # If values are provided, update the fit_report.
     def evaluate_residual(self,q,I,dI=None,I_comp=None):
         """Evaluate the fit residual for a given populations dict.
     
